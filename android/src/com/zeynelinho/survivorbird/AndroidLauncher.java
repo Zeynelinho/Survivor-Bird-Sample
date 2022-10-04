28ec363766110b54d928a5d50daa282e1894138b
Zeynelinho <zeynelycl@gmail.com>
2022-10-04 15:15:24 +0300

    Initial commit

---
+++ android/src/com/zeynelinho/survivorbird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.zeynelinho.survivorbird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.zeynelinho.survivorbird.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
